ca1e60f49753087080b348f52d7fd6d8a9d045f4
marc25219 <marc25219@gmail.com>
2024-03-03 00:18:13 +0100

    Add Handling objects as arrays

--- 08.Arrays/7.Object-to-array.js
+++ 08.Arrays/7.Object-to-array.js
@@ -4,47 +4,41 @@
  * Description: Object to Array => Use of Object.keys(), Object.values() and Object.entries()
  */
 
-
 const book = {
-    id: 1,
-    title: "Thinking, fast and slow",
-    author: "Daniel Kahneman",
-    price: 12.5,
-    format: "paperback",
-    genres: ["Education", "Psychology"],
-    pages: 731,
-  }
+  id: 1,
+  title: "Thinking, fast and slow",
+  author: "Daniel Kahneman",
+  price: 12.5,
+  format: "paperback",
+  genres: ["Education", "Psychology"],
+  pages: 731,
+};
 
 // Object.keys()
-const properties = Object.keys(book)
+const properties = Object.keys(book);
 
-console.log(properties.length) // 7
+console.log(properties.length); // 7
 
-properties.forEach(property => {
-
+properties.forEach((property) => {
   // id, title, author...
-  console.log("Object.keys(): " + property) 
-})
-
+  console.log("Object.keys(): " + property);
+});
 
 // Object.values()
-const values = Object.values(book)
+const values = Object.values(book);
 
-values.forEach(value => {
-
+values.forEach((value) => {
   // 1, "Thinking, fast and slow", ..., ["Education", "Psychology"]...
-  console.log("Object.values(): " + value) 
-})
-
+  console.log("Object.values(): " + value);
+});
 
 // Object.entries() => returns a bidimensional array (key, value)
-const entries = Object.entries(book)
+const entries = Object.entries(book);
 
-console.log("")
-console.log("Object.entries(): ") 
+console.log("");
+console.log("Object.entries(): ");
 
-entries.forEach(entry => {
-
+entries.forEach((entry) => {
   // ['id', 1], ['title', 'Thinking, fast and slow']
-  console.log(entry) 
-})
+  console.log(entry);
+});
